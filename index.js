28f48ad3e03a7ba206a5a934bbc56c412f5b27f8
Limber Cedeño <lcedeno6082@utm.edu.ec>
2021-02-25 02:17:54 -0500

    asasq

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
@@ -11,3 +12,37 @@ app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}`)
 })
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+/* const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+
+//Database Connection
+const db = require('./config/database');
+db.authenticate().then(() => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Error: ' + err);
+})
+
+const app = express();
+
+app.use('/', require('./routes/routes'));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(cors("*"));
+
+const PORT = process.env.PORT || 5000;
+db.sync().then(() => {
+    app.listen(PORT, console.log(`Server started on port ${PORT}`));
+}).catch(err => console.log("Error: " + err));
+ */
